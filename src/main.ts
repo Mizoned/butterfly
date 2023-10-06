7d54f9f9b23472b3e1b328fc9dd0ddd18fadb0a1
Valery Shcherbinin <fundroin@mail.ru>
2023-10-06 09:38:04 +0500

    56 добавляет router, создает страницу с ui компонентами и базовый компонент кнопки

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import './style.css';
-import 'normalize.css';
+import router from './router';
 import App from './App.vue';
 
 // @Styles
@@ -10,5 +9,4 @@ import '@/assets/styles/main.scss';
 const pinia = createPinia();
 const app = createApp(App);
 
-app.use(pinia);
-app.mount('#app');
+app.use(router).use(pinia).mount('#app');
