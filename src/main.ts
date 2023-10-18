ff3bb0104e8a739a659dea3298e0129bd511ef93
Valery Shcherbinin <fundroin@mail.ru>
2023-10-18 12:23:58 +0500

    80 добавляет ui библиотеку ant design

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
+import Antd from 'ant-design-vue';
 
 // @Styles
 import '@/assets/styles/main.scss';
 
-const pinia = createPinia();
 const app = createApp(App);
 
-app.use(router).use(pinia).mount('#app');
+app.use(Antd).use(router).use(createPinia()).mount('#app');
